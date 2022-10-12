a340924a78b5b4a0ffbc0bc952c992c019faad0e
unknown <nayeemhaque43@gmail.com>
2022-10-12 20:08:04 +0600

    blog question1

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>What is the purpose of react router?</h1>
+
+      <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+      
+      
+    </div>
+
+  
+  );
+};
+
+export default Blog;
