86f4314e1e06164c563e1151cb1910ca4aaaf087
unknown <nayeemhaque43@gmail.com>
2022-10-12 20:09:15 +0600

    blog question2

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,7 +7,14 @@ const Blog = () => {
 
       <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
       
+      <h1> How does context API work?</h1>
+
+      <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease. 
+      React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
+      </p>
+
       
+
     </div>
 
   
