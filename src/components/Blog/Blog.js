3437be75d8ef33999f3d7f71a421b017cbc2b210
unknown <nayeemhaque43@gmail.com>
2022-10-12 20:11:06 +0600

    blog update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -13,7 +13,9 @@ const Blog = () => {
       React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
       </p>
 
-      
+      <h1> What is useRef and how do you use it?</h1>
+      <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React, React will set its .current property to the corresponding DOM node whenever that node changes.The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.
+      </p>
 
     </div>
 
